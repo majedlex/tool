dbb63edb9c305cb514f5c026576c869e02429501
leo <zongmingli@outlook.com>
2020-03-27 09:09:07 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,15 +11,15 @@ do
 	if [[ ${arr[$loop]} =~ $Hynix ]]
 	then
         echo find Hynix Mem
-        bash /home/work/tool/setPPT.sh $loop /home/work/tool/V56PPT965910800
-		/home/work/tool/amdmemtweak --i $loop --REF 17000 --RCDRD 21 --rc 38 --rp 14 --ras 24 --rfc 176
+        bash /home/work/tool/setPPT.sh $loop /home/work/tool/V56PPT965900800
+		/home/work/tool/amdmemtweak --i $loop --REF 17000 --RCDRD 21
 	elif [[ ${arr[$loop]} =~ $Samsung ]]
 	then
 		echo find Samsung Mem
-        bash /home/work/tool/setPPT.sh $loop /home/work/tool/V56PPT965910800
+        bash /home/work/tool/setPPT.sh $loop /home/work/tool/V56PPT965900800
 		/home/work/tool/amdmemtweak --i $loop --REF 12000 --RCDRD 12
 	else
-		bash /home/work/tool/setPPT.sh $loop /home/work/tool/V56PPT965910800
+		bash /home/work/tool/setPPT.sh $loop /home/work/tool/V56PPT965900800
 		/home/work/tool/amdmemtweak --i $loop --REF 15600
 	fi
 }
